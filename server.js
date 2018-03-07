0b1bc8aa3c934d01257cf8eb3ee569045e359410
Matthew P. Del Buono <mpdelbuono@gmail.com>
2018-03-07 01:13:11 -0800

    Initial kanabot framework established. Only connects to discord right now and logs the successful connection. Token is specified via 'npm config' setting 'token'.

---
+++ server.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log('System connected: ' + client.user.tag)
+});
+
+// Note: discord.js provides us no mechanism with which to catch the failed
+// login. However, the bot will terminate with a meaningful error, so we'll
+// just take that for now.
+const token = client.login(process.env.npm_package_config_token)
